51e7159c60e94cf00d177a8b2c78ab58bf2881b9
natsu <natsu@DESKTOP-A2UNBDV>
2021-11-23 02:28:59 +0630

    third

--- LoginAssign/src/main/java/com/loginAssign/user/controller/HomeController.java
+++ LoginAssign/src/main/java/com/loginAssign/user/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	// git add
+	// third
 	@RequestMapping("/")
 	private String home() {
        return "index";
